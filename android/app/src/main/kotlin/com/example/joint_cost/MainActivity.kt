271c6340755893afd030b4d6fe09a418aedbd666
takezawa <s.take4173@gmail.com>
2024-02-08 12:33:06 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/joint_cost/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.joint_cost
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:intl/date_symbol_data_local.dart';
+
+DateTime _focusedDay = DateTime.now();
+void main() {
+  initializeDateFormatting('ja').then((_) => runApp(MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter title',
+      theme: ThemeData(),
+      home: const MyHomePage(title: 'Flutter Home'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Padding(
+      padding: EdgeInsets.only(top: 50),
+      child: Center(
+        child: TableCalendar(
+            firstDay: DateTime.utc(2024, 1, 1),
+            lastDay: DateTime.utc(2028, 12, 31),
+            focusedDay: _focusedDay,
+            locale: 'ja_Jp'),
+      ),
+    ));
+  }
+}
